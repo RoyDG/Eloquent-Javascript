b72e38cf4f48aa1e9eae4e0e9f84d2318dd67f91
unknown <rdg01834@gmail.com>
2021-09-03 23:49:46 +0600

    value,type operator

---
+++ chapter1.js
@@ -0,0 +1,32 @@
+/* VALUE, TYPE, OPERATOR */
+
+13 - Number
+13.998 - INTEGER
+13.998e8 (exponent) = 13.998 * 10^8
+`hello` , "hello" , 'hello' - String
+"con" + "catenate" - concatenate
+`half of 100 is ${100/2}` - template_literal
+typeof , (with-single-operator) -8 - Unary_operator
+! = Not (flip_the_value )
+!= - not_equal_to
+
+And = 
+    console.log(true && false) = false,
+    console.log(true && true) = true
+OR =
+    console.log(false || true) = true,
+    console.log(false || false) = false
+Ternary
+    console.log(true ? 1:2) = 1,
+    console.log(false ? 1:2) = 2
+
+null, undefined - empty_value
+
+Autometic_type_conversion
+    console.log(8 * null) = 0,
+    console.log("5" - 1) = 4,
+    console.log("5" + 1) = 51,
+    console.log("five" * 2) = NaN,
+    console.log(false == 0) = true,
+    console.log(null == undefined) = true,
+    console.log(null == 0) = false
